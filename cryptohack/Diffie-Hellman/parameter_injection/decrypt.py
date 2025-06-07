42f9d85a4f24793125d594b3543d5193b18b80a0
David Jiang <djiang56@protonmail.com>
2025-06-07 11:37:51 -0400

    .

--- cryptohack/Diffie-Hellman/parameter_injection/decrypt.py
+++ cryptohack/Diffie-Hellman/parameter_injection/decrypt.py
@@ -1,5 +1,5 @@
 from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad, unpad
+from Crypto.Util.Padding import unpad
 import hashlib
 from pwn import *
 
@@ -9,16 +9,15 @@ PORT = 13371
 remote(HOST, PORT)
 
 
-
 def is_pkcs7_padded(message):
-    padding = message[-message[-1]:]
+    padding = message[-message[-1] :]
     return all(padding[i] == len(padding) for i in range(0, len(padding)))
 
 
 def decrypt_flag(shared_secret: int, iv: str, ciphertext: str):
     # Derive AES key from shared secret
     sha1 = hashlib.sha1()
-    sha1.update(str(shared_secret).encode('ascii'))
+    sha1.update(str(shared_secret).encode("ascii"))
     key = sha1.digest()[:16]
     # Decrypt flag
     ciphertext = bytes.fromhex(ciphertext)
@@ -27,9 +26,9 @@ def decrypt_flag(shared_secret: int, iv: str, ciphertext: str):
     plaintext = cipher.decrypt(ciphertext)
 
     if is_pkcs7_padded(plaintext):
-        return unpad(plaintext, 16).decode('ascii')
+        return unpad(plaintext, 16).decode("ascii")
     else:
-        return plaintext.decode('ascii')
+        return plaintext.decode("ascii")
 
 
 # shared_secret = ?

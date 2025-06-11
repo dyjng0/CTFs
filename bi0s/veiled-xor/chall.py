57572c30d036b079196c3c1bec483d7db29d7ec8
David Jiang <djiang56@protonmail.com>
2025-06-10 20:42:22 -0400

    .

--- bi0s/veiled-xor/chall.py
+++
@@ -1,6 +0,0 @@
-from Crypto.Util.number import bytes_to_long, getPrime
-
-n = (p := getPrime(1024)) * (q := getPrime(1024))
-print(
-    f"n : {n}\nc : {pow(bytes_to_long(flag), 65537, n)}\nVeil XOR: {p ^ int(bin(q)[2:][::-1], 2)}"
-)

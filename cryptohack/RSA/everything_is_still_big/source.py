520ab15e09b5492701632663810ab1be3e07fde4
David <djiang56@protonmail.com>
2025-07-11 16:52:50 -0400

    .

--- cryptohack/RSA/everything_is_still_big/source.py
+++ cryptohack/RSA/everything_is_still_big/source.py
@@ -8,22 +8,23 @@ FLAG = b"crypto{?????????????????????????????????????}"
 
 m = bytes_to_long(FLAG)
 
+
 def get_huge_RSA():
     p = getPrime(1024)
     q = getPrime(1024)
-    N = p*q
-    phi = (p-1)*(q-1)
+    N = p * q
+    phi = (p - 1) * (q - 1)
     while True:
         d = getrandbits(512)
-        if (3*d)**4 > N and gcd(d,phi) == 1:
+        if (3 * d) ** 4 > N and gcd(d, phi) == 1:
             e = inverse(d, phi)
             break
-    return N,e
+    return N, e
 
 
 N, e = get_huge_RSA()
 c = pow(m, e, N)
 
-print(f'N = {hex(N)}')
-print(f'e = {hex(e)}')
-print(f'c = {hex(c)}')
+print(f"N = {hex(N)}")
+print(f"e = {hex(e)}")
+print(f"c = {hex(c)}")

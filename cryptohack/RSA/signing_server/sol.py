784f79f87bf896b6c6038fa511671b07822b73b1
David <djiang56@protonmail.com>
2025-07-29 12:28:21 -0400

    Solved signing_server.

--- cryptohack/RSA/signing_server/sol.py
+++ cryptohack/RSA/signing_server/sol.py
@@ -1,9 +1,33 @@
 from Crypto.Util.number import long_to_bytes
-from pwn import *
-from json import *
+from pwn import remote
+import json
 
 HOST = "socket.cryptohack.org"
 PORT = 13374
+r = remote(HOST, PORT)
+r.recvline()
 
-remote(HOST, PORT)
 
+def get_pubkey():
+    message = json.dumps({"option": "get_pubkey"})
+    r.sendline(message.encode())
+    pubkey = json.loads(r.recvline().decode("utf8"))
+    return int(pubkey["N"], 16), int(pubkey["e"], 16)
+
+
+def get_secret():
+    message = json.dumps({"option": "get_secret"})
+    r.sendline(message.encode())
+    secret_json = json.loads(r.recvline().decode("utf8"))
+    return secret_json["secret"]
+
+
+def sign(msg):
+    message = json.dumps({"option": "sign", "msg": msg})
+    r.sendline(message.encode())
+    msg_json = json.loads(r.recvline().decode("utf8"))
+    return int(msg_json["signature"], 16)
+
+
+secret = get_secret()
+print(long_to_bytes(sign(secret)))

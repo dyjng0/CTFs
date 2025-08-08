47b7a9f9d1a96efe0b36df3bef7b973ae28c8b78
David <djiang56@protonmail.com>
2025-08-08 15:26:21 -0400

    Moved decrypt.py

--- cryptohack/Diffie-Hellman/decrypt.py
+++ cryptohack/Diffie-Hellman/decrypt.py
@@ -1,17 +1,17 @@
 from Crypto.Cipher import AES
-from Crypto.Util.Padding import unpad
+from Crypto.Util.Padding import pad, unpad
 import hashlib
 
 
 def is_pkcs7_padded(message):
-    padding = message[-message[-1] :]
+    padding = message[-message[-1]:]
     return all(padding[i] == len(padding) for i in range(0, len(padding)))
 
 
 def decrypt_flag(shared_secret: int, iv: str, ciphertext: str):
     # Derive AES key from shared secret
     sha1 = hashlib.sha1()
-    sha1.update(str(shared_secret).encode("ascii"))
+    sha1.update(str(shared_secret).encode('ascii'))
     key = sha1.digest()[:16]
     # Decrypt flag
     ciphertext = bytes.fromhex(ciphertext)
@@ -20,13 +20,8 @@ def decrypt_flag(shared_secret: int, iv: str, ciphertext: str):
     plaintext = cipher.decrypt(ciphertext)
 
     if is_pkcs7_padded(plaintext):
-        return unpad(plaintext, 16).decode("ascii")
+        return unpad(plaintext, 16).decode('ascii')
     else:
-        return plaintext.decode("ascii")
+        return plaintext.decode('ascii')
 
 
-# shared_secret = ?
-# iv = ?
-# ciphertext = ?
-
-# print(decrypt_flag(shared_secret, iv, ciphertext))

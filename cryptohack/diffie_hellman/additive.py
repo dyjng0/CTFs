5d0cce047fb40c0510b3a6bad3476c15b3d3ff88
David <djiang56@protonmail.com>
2025-08-12 22:44:48 -0400

    Solved additive.

--- cryptohack/diffie_hellman/additive.py
+++ cryptohack/diffie_hellman/additive.py
@@ -24,27 +24,15 @@ def A_ciphertext(io):
     return iv, ct
 
 
-def diophantine(a, b, c):
-    q = a // b
-    r = a % b
-    if r == 0:
-        return [0, c // b]
-    else:
-        sol = diophantine(b, r, c)
-        u = sol[0]
-        v = sol[1]
-        return [v, u - q * v]
-
-
 def solve(io):
     p, g, A = A_pubkey(io)
     B = B_pubkey(io)
     iv, ct = A_ciphertext(io)
     io.close()
-    a = diophantine(g, p, A)[0]
-    b = diophantine(g, p, B)[0]
-    print(a, b)
-    shared_secret = g * (a + b) % p
+    g_inv = pow(g, -1, p)
+    a = (A * g_inv) % p
+    b = (B * g_inv) % p
+    shared_secret = g * (a * b) % p
     print(decrypt_flag(shared_secret, iv, ct))
     return
 
